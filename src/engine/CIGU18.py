4c81163f70c3dcbf35ae3e9f1b57a36cff9d1c84
Andrea Stefan <andreastefan0101@gmail.com>
2025-04-19 17:47:32 +0200

    Added support to query with the name + last name for CIGu18

--- src/engine/CIGU18.py
+++ src/engine/CIGU18.py
@@ -10,9 +10,9 @@ async def query(logInt: logger) -> List[List[str]]:
     url = "https://www.federscacchi.it/giova/finalisti.php?azione=2"
     qualified = []
     text = None
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url, timeout=aiohttp.ClientTimeout(total=180)) as response:
-
             if response.status < 200 or response.status > 299:
                 text = await response.text()
                 await logInt.error(f"UNEXPECTED STATUS CODE: {response.status}\n{text}", shouldExit=True)
@@ -20,8 +20,12 @@ async def query(logInt: logger) -> List[List[str]]:
             text = await response.text()
 
     parsedText = csv.reader(io.StringIO(text), delimiter=";")
+    nameParts = re.split(r'\s+', settings.queryName.strip().lower())
+
     for row in parsedText:
-        if re.search(re.escape(settings.queryName), row[1], re.IGNORECASE):
+        playerName = row[1].lower()
+
+        if all(re.search(re.escape(part), playerName, re.IGNORECASE) for part in nameParts):
             qualified.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
 
     await logInt.apiRequest(url, {}, text)
