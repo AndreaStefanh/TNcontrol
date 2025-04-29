fb5456dafafed51fcb71e88104d0fd17a5929ee8
Andrea Stefan <andreastefan0101@gmail.com>
2025-04-29 14:01:49 +0200

    Got event ids for every tournament in vegaresult

--- src/engine/vegaresult.py
+++ src/engine/vegaresult.py
@@ -3,21 +3,38 @@
 # when it reaches a semi-functional state it can be executed by tncontrol
 import requests
 import json
+from bs4 import BeautifulSoup
 
+def request(param: str) -> str:
+    r = requests.get("https://www.vegaresult.com/vr/" + param)
+    return r.text
+
+def getIds() -> list[str]:
+    tournamentsInfo = request("get_tournaments.php?status=next&timecontrol=all&interest=all&type=all&event=&startdate=&enddate=")
+    tournamentsInfo = json.loads(tournamentsInfo)["tournaments"]
+
+    ids = []
+    for tornament in tournamentsInfo:
+        ids.append(tornament["id"])
+    
+    return ids
+
+def getEventId() -> None:
+
+    ids = getIds()
+    for id in ids:
+        tournamentPage = request(id)
+        soup = BeautifulSoup(tournamentPage, "html.parser")
+        
+        for link in soup.find_all('a'):
+            if link.get("href").startswith("players.php?section_id="):
+                print(link.get("href"))
+    
+    return
 
 def main() -> None:
     
-    url = "https://www.vegaresult.com/vr/"
-
-    r = requests.get(url + "get_tournaments.php?status=next&timecontrol=all&interest=all&type=all&event=&startdate=&enddate=")
-    data = json.loads(r.text)["tournaments"]
-
-    ids = []
-
-    for tornament in data:
-        ids.append(tornament["id"])
-    
-    print(ids)
+    getEventId()
     return 
 
 if __name__ == "__main__":
