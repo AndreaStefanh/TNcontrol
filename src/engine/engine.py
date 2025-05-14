bc297e78d6fdee296b051d415743123fe4d3c4b2
Andrea Stefan <andreastefan0101@gmail.com>
2025-05-14 13:03:48 +0200

    Removed print used for debug

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -14,7 +14,5 @@ async def start(logInt: logger) -> List[List[Union[Dict[str, Union[str, Dict[str
         if settings.selectedEngine & engineFlags.VEGARESULT:    tasks.append(tg.create_task(vegaresult.query(logInt)))
         if settings.selectedEngine & engineFlags.CIGU18:        tasks.append(tg.create_task(CIGU18.query(logInt)))
     result = [task.result() for task in tasks]
-    print("\n" + str(result))
-    exit(0)
 
     return result
